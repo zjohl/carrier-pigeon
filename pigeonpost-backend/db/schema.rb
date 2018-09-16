cff9b7bf3c8e469431b1ebd5d542c7dfd5acbd2d
Michael Wong <emporophobe@gmail.com>
2018-09-16 13:42:31 -0700

    Add drones table

--- pigeonpost-backend/db/schema.rb
+++ pigeonpost-backend/db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_16_193125) do
+ActiveRecord::Schema.define(version: 2018_09_16_202323) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "drones", force: :cascade do |t|
+    t.float "latitude"
+    t.float "longitude"
+    t.float "destinationLatitude"
+    t.float "destinationLongitude"
+    t.string "status"
+    t.integer "batteryPercent"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
