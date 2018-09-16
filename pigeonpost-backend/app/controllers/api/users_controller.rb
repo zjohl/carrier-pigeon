871ee36c14842449412a6d848693470cb007520b
Zamir Johl <zjohl@pivotal.io>
2018-09-16 16:01:39 -0400

    Look up users by id

--- pigeonpost-backend/app/controllers/api/users_controller.rb
+++ pigeonpost-backend/app/controllers/api/users_controller.rb
@@ -8,7 +8,7 @@ module Api
 
     def show
       render partial: "shared/json/user.json", locals: {
-          users: User.find(params[:email]),
+          users: User.find(params[:id]),
       }
     end
 
